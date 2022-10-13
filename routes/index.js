4acb2dbb2414269a9c6e027ed3501bd1078dd3e4
kimhengpeng <kpeng@sfsu.edu>
2022-10-12 19:59:48 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const app = express();
-const port = 3001;
 
-app.listen(port, () => console.log('App listening to post ${port}'));
-
-app.get('/', (req, res) => res.send('Hello World!'));
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('Hello World');
 });
 
 module.exports = router;
