044c805c71516c93af6e8451884dcc4481ea269f
kimhengpeng <kpeng@sfsu.edu>
2022-11-08 20:21:42 -0800

    Create hands.js

---
+++ handler/hands.js
@@ -0,0 +1,29 @@
+
+let dealerCards = [
+  {rank: 9, suit: "S"}, {rank: 5, suit: "S"}, {rank: 1, suit: "S"}, {rank: 11, suit: "S"}, {rank: 6, suit: "S"}
+]
+
+let playerCards = [
+  {rank: 13, suit: "S"}, {rank: 1, suit: "S"} 
+]
+
+let sortCards = (cards) => {
+  //Change Ace from rank 1 to rank 14 highest priority
+  cards.forEach((card) => {
+    if(card.rank == 1){
+      card.rank = 14;
+    }
+  })
+  //Sort card base on their ranks
+  cards.sort(function(a, b){
+    return a.rank - b.rank;
+  })
+}
+
+let calculateHands = (dealerCards, playerCards) => {
+  let score = 0;
+  let tempCards = dealerCards.concat(playerCards);
+  
+}
+
+calculateHands(dealerCards, dealerCards);
