117304276f0a1dd0abcbec79f64469c2dd4066cb
kimhengpeng <kpeng@sfsu.edu>
2022-11-08 21:10:13 -0800

    Update hands.js

--- handler/hands.js
+++ handler/hands.js
@@ -1,10 +1,10 @@
 
 let dealerCards = [
-  {rank: 9, suit: "S"}, {rank: 5, suit: "S"}, {rank: 1, suit: "S"}, {rank: 11, suit: "S"}, {rank: 6, suit: "S"}
+  {rank: 9, suit: "S"}, {rank: 10, suit: "S"}, {rank: 1, suit: "S"}, {rank: 11, suit: "S"}, {rank: 12, suit: "S"}
 ]
 
 let playerCards = [
-  {rank: 13, suit: "S"}, {rank: 1, suit: "A"} 
+  {rank: 13, suit: "S"}, {rank: 1, suit: "D"} 
 ]
 
 
@@ -26,7 +26,53 @@ let calculateHands = (dealerCards, playerCards) => {
   let score = 0;
   let tempCards = dealerCards.concat(playerCards);
   sortCards(tempCards);
+
+
+
   console.log(tempCards);
+  checkRoyalFlush(tempCards);
+
+  return score;
 }
+
+let checkRoyalFlush = (sortedCards) => {
+  let score = 0;
+  // 10, 11, 11, 12, 13, 14
+  let isRoyalFlush = false;
+  let index = 10;
+
+  let myMap = new Map();
+  myMap.set(10, 0);
+  myMap.set(11, 0);
+  myMap.set(12, 0);
+  myMap.set(13, 0);
+  myMap.set(14, 0);
+
+ sortedCards.forEach((card) => {
+  //console.log(card.rank);
+  if(myMap.has(card.rank)){
+    myMap.set(card.rank, myMap.get(card.rank) + 1);
+  }
+ })
+
+  console.log(myMap);
+
+}
+
+//Royal Flush 10*1000
+//Straight Flush 9*1000
+//Four of A Kind 8*1000
+//Full House 7*1000
+//Flush 6*1000
+//Straight 5*1000
+//Three of Kind 4*1000
+//Two pair 3*1000
+//Pair 2*1000
+//Highest Hand 1*1000
+
+
+//2000 + 5*10 + 4
+//2000 + 6*10 + 3
+
 
 calculateHands(dealerCards, playerCards);
