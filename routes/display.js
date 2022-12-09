5414737edce8965f76dd8c84365c233fb3bb96cb
Abdul Barrie <asbarrie14@yahoo.com>
2022-12-08 17:14:48 -0800

    Worked on CSS, Hbars and JS for registration and about pages.

---
+++ routes/display.js
@@ -0,0 +1,10 @@
+/*This function allows the user to click the search icon to make the
+Message on the About Page appear.*/
+function messageAppear() {
+    var x = document.getElementById("Message");
+    if (x.style.display == "grid") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "grid";
+    }
+  }
