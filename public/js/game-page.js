ad279a1200f97b34616349a7498fe786849be1b4
kimhengpeng <kpeng@sfsu.edu>
2022-12-09 08:28:39 -0800

    game chat working

--- public/js/game-page.js
+++ public/js/game-page.js
@@ -1,10 +1,12 @@
+
+let pathname = window.location.pathname;
+const pathnameSegments = pathname.split('/');
+const gameId = pathnameSegments.pop();
+
 let leaveButton = document.getElementById("leave-game");
 
 leaveButton.onclick = () => {
-  let pathname = window.location.pathname;
-  const pathnameSegments = pathname.split('/');
-  const lastElement = pathnameSegments.pop();
-  fetch(`/api/lobby/leave/${lastElement}`, {method: "post"})
+  fetch(`/api/lobby/leave/${gameId}`, {method: "post"})
   .then((result) => {
     return result.json();
   })
@@ -15,3 +17,38 @@ leaveButton.onclick = () => {
   })
   .catch(err => console.log(err));
 }
+
+
+
+const sendMessage = document.getElementById(`send-message-button-${gameId}`);
+
+
+sendMessage.onclick = () => {
+  let message = document.getElementById(`send-message-text-${gameId}`);
+  
+  fetch(`/api/chat/${gameId}`, {
+    method: "post",
+    headers: {'Content-Type' : "application/json"},
+    body: JSON.stringify({message: message.value}),
+  })
+    .then(() =>{
+      message.value = "";
+    })
+    .catch((err) => console.log(err));
+};
+
+socket.on(`chat:${gameId}`, ({sender, message, timestamp}) => {
+  console.log({sender, message, timestamp});
+
+  const div = document.createElement("div");
+  div.classList.add("message");
+  const content = document.createElement("p");
+
+  let str = sender + ": " + message;
+  content.innerText = str;
+  div.appendChild(content);
+
+  let chatBox = document.getElementById(`chat-${gameId}`);
+  chatBox.appendChild(div);
+
+})
