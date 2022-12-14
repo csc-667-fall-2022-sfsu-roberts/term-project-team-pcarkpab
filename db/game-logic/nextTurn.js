73062b68a755cfa22aaf41f3b940482cb6d79a62
kimhengpeng <kpeng@sfsu.edu>
2022-12-14 15:51:54 -0800

    Fix nextTurn for folded players

--- db/game-logic/nextTurn.js
+++ db/game-logic/nextTurn.js
@@ -12,6 +12,7 @@ const nextTurn = async (gameId, isTurn) => {
     // If the current turn is the last player, set the turn to the first player (with seat number 0)
     if (result.count == (isTurn + 1)) {
       console.log("next Turn: 0");
+      isTurn = 0;
       await Games.setPlayerTurn(0, gameId);
     } else {
       // Otherwise, set the turn to the next player
@@ -21,10 +22,12 @@ const nextTurn = async (gameId, isTurn) => {
     }
 
     // Check the status of the player with the current seat number
+    console.log(gameId + ', ' + isTurn);
     const playerStatus = await Games.checkPlayerStatusWithSeatNum(gameId, isTurn);
-
+    console.log("Debug");
+    console.log(playerStatus);
     // If the player is not in the "FOLD" state, we have found the next active player
-    if (playerStatus.status !== 'FOLD') {
+    if (playerStatus[0].status !== 'FOLD') {
       break;
     }
   }
